971b43342b7480d7b3abf9572d9fd4c4e48d4764
Chandana A N <125329200+arikac01@users.noreply.github.com>
2023-02-23 15:15:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,44 @@ function Game(){
   let random2 = Math.floor(Math.random() * 3) + 1; // 1-3
 
   // Assign the appropriate image
-  let ImageSource1 = "img/rock.png"; // imgs/rock.png or  imgs/scissors.png or  imgs/paper.png
+  let randomImageName1 = "rock.png" // Update this randomly
+  let ImageSource1 = "" + randomImageName1; // imgs/rock.png or  imgs/scissors.png or  imgs/paper.png
+
+  let randomImageName2 = "hand.png" // Update this randomly
+  let ImageSource2 = "" + randomImageName2; // imgs/rock.png or  imgs/scissors.png or  imgs/paper.png
+
+  let randomImageName3 = "hand.png" // Update this randomly
+  let ImageSource3 = "" + randomImageName3; // imgs/rock.png or  imgs/scissors.png or  imgs/paper.png
+
+  
+
+  // Update images
+  //document.querySelectorAll("img")[0].setAttribute("src", "img/hand.png");
+  //document.querySelectorAll("img")[0].setAttribute("src", ImageSource1);
+  document.querySelectorAll("img")[1].setAttribute("src", ImageSource1);
+  document.querySelectorAll("img")[2].setAttribute("src", ImageSource2);
+  document.querySelectorAll("img")[3].setAttribute("src", ImageSource3);
+  
+// Print the winner. Use if statement or other appropriate logic.
+  
+  if ((random1 == 1 && random2 == 1) || (random1 == 2 && random2 == 2) ||(random1 == 3 && random2 == 3)){
+  document.querySelector("h1").innerHTML = "It's a Tie!";}
+  else if (random1 == 1 && random2 == 2){
+  document.querySelector("h1").innerHTML = "Play 2 Wins!";}
+  else if (random1 == 2 && random2 == 3){ 
+  document.querySelector("h1").innerHTML = "Play 2 Wins!";}
+  else if (random1 == 1 && random2 == 3){
+  document.querySelector("h1").innerHTML = "Play 1 Wins!";} 
+  else if (random1 == 2 && random2 == 1){
+  document.querySelector("h1").innerHTML = "Play 1 Wins!";}  
+  else if (random1 == 3 && random2 == 2){
+  document.querySelector("h1").innerHTML = "Play 1 Wins!";}
+  else if (random1 == 3 && random2 == 1){
+  document.querySelector("h1").innerHTML = "Play 2 Wins!";}
+}
+
+
+/*let ImageSource1 = "img/rock.png"; // imgs/rock.png or  imgs/scissors.png or  imgs/paper.png
   let ImageSource2 = "img/hand.png";
   let ImageSource3 = "img/scissors.png";  
 
@@ -38,19 +75,3 @@ function Game(){
   /*document.querySelectorAll("img")[2].setAttribute("src", ImageSource2);
   document.querySelectorAll("img")[3].setAttribute("src", ImageSource3);
 */
-  // Print the winner. Use if statement or other appropriate logic.
-  if ((random1 == 1 && random2 == 1) || (random1 == 2 && random2 == 2) ||(random1 == 3 && random2 == 3)){
-  document.querySelector("h1").innerHTML = "It's a Tie!";}
-  else if (random1 == 1 && random2 == 2){
-  document.querySelector("h1").innerHTML = "Play 2 Wins!";}
-  else if (random1 == 2 && random2 == 3){
-  document.querySelector("h1").innerHTML = "Play 2 Wins!";}
-  else if (random1 == 1 && random2 == 3){
-  document.querySelector("h1").innerHTML = "Play 1 Wins!";} 
-  else if (random1 == 2 && random2 == 1){
-  document.querySelector("h1").innerHTML = "Play 1 Wins!";}  
-  else if (random1 == 3 && random2 == 2){
-  document.querySelector("h1").innerHTML = "Play 1 Wins!";}
-  else if (random1 == 3 && random2 == 1){
-  document.querySelector("h1").innerHTML = "Play 2 Wins!";}
-}
